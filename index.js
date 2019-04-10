4f747637315e25ca9aaa7eebe0f34ec1a9ab9585
moranja <jeffadammoran@gmail.com>
2019-04-09 22:55:19 -0500

    done

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objectArray) {
+  let win = objectArray.find( o => o.result === "W")
+  if (win === undefined) {
+    return undefined
+  } else {
+    return win.year
+  }
+}
